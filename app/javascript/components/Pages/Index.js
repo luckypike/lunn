89916505b8bd5bb7d42a6b5cdd332fb3954fd8cc
Andy Braga <ab@luckypike.com>
2019-11-22 12:57:08 +0300

    Пустая Index страница

---
+++ app/javascript/components/Pages/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+import styles from './Index.module.css'
+
+export default function Index () {
+  return (
+    <div className={styles.root}>
+      INDEX
+    </div>
+  )
+}
