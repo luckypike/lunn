dbc092ad1bc33f220462c38f6f267716ecf1b7f7
Maxim <max.spiridonov@gmail.com>
2020-01-09 12:12:06 +0300

    Убран скролл у табов + добавлены фото для вложенных категорий

--- app/javascript/components/Navs.js
+++ app/javascript/components/Navs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import styles from './Navs.module.css'
 
@@ -11,7 +12,7 @@ export default function Navs ({ navs }) {
   return (
     <div className={styles.items}>
       {navs.map((nav, i) =>
-        <a href={nav.path} key={nav.mlid} className={styles.item}>
+        <a href={nav.path} key={nav.mlid} className={classNames(styles.item, styles[`image_${Math.floor(Math.random() * 6) + 1}`])}>
           <div className={styles.title}>
             {nav.title}
           </div>
