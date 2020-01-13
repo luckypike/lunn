03f509e8c2df44b694f6b1d59a53d585d19f4edb
Andy Braga <ab@luckypike.com>
2020-01-13 17:17:00 +0300

    Добавлена иконка для внешних ссылок

--- app/javascript/components/Pages/Index/Sec.js
+++ app/javascript/components/Pages/Index/Sec.js
@@ -44,6 +44,13 @@ function Items ({ items }) {
       {items.map((item, i) =>
         <a href={item.path} key={i} className={classNames(styles.item, styles[`image_${Math.floor(Math.random() * 6) + 1}`])}>
           <div className={styles.title}>{item.title}</div>
+
+          {item.external &&
+            <svg className={styles.external} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 21L20.9998 1.00027" strokeWidth="1.5"></path>
+              <path d="M6 1L20.9998 1V15.9998" strokeWidth="1.5"></path>
+            </svg>
+          }
         </a>
       )}
     </>
