7486059cead405185ca288d588a2ac6c149fc40a
Andy Braga <ab@luckypike.com>
2020-01-13 17:10:55 +0300

    Изменен рендерер

--- app/javascript/components/Draft.js
+++ app/javascript/components/Draft.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import redraft from 'redraft'
+import classNames from 'classnames'
+import parse, { domToReact } from 'html-react-parser'
 
 import { ContentState, convertFromHTML, convertToRaw } from 'draft-js'
 
@@ -11,56 +13,174 @@ Renderer.propTypes = {
 }
 
 export default function Renderer ({ source }) {
-  const blocks = convertFromHTML(source)
-  const state = ContentState.createFromBlockArray(
-    blocks.contentBlocks,
-    blocks.entityMap
-  )
+  const content = parse(source, options)
 
-  return redraft(convertToRaw(state), renderers)
+  return content
 }
 
-const getParagraphs = (children, { keys }) =>
-  children.map((p, i) =>
-    <p key={keys[i]} className={styles.p}>
-      {p}
-    </p>
-  )
+const options = {
+  replace: ({ type, name, attribs, children, data, parent }) => {
+    // console.log(type)
+    // console.log(name)
+    // console.log(children)
+    // console.log(data)
 
-const getH2 = (children, { keys }) =>
-  children.map((p, i) =>
-    <h2 key={keys[i]} className={styles.h2}>
-      {p}
-    </h2>
-  )
+    // if (!attribs) return
 
-const getUl = (children, { depth, keys }) =>
-  <ul key={keys.join('-')} className={styles.ul}>
-    {children.map((li, i) =>
-      <li key={keys[i]}>{li}</li>
-    )}
-  </ul>
+    if (type === 'tag') {
+      if (name === 'p') {
+        if (children.length === 1 && children[0].data && children[0].data.replace(/\s/g, '') === '') {
+          return <></>
+        }
 
-const getOl = (children, { depth, keys }) =>
-  <ol key={keys.join('-')} className={styles.ol}>
-    {children.map((li, i) =>
-      <li key={keys[i]}>{li}</li>
-    )}
-  </ol>
+        return (
+          <p className={classNames(styles.p, styles[attribs.class])}>
+            {domToReact(children, options)}
+          </p>
+        )
+      }
 
-const getStyle = (children, { key }) =>
-  <strong key={key}>
-    {children}
-  </strong>
+      if (name === 'div') {
+        if (children.length === 1 && children[0].data && children[0].data.replace(/\s/g, '') === '') {
+          return <></>
+        }
 
-const renderers = {
-  inline: {
-    BOLD: getStyle
-  },
-  blocks: {
-    unstyled: getParagraphs,
-    'header-two': getH2,
-    'unordered-list-item': getUl,
-    'ordered-list-item': getOl
+        return (
+          <div className={styles[attribs.class]}>
+            {domToReact(children, options)}
+          </div>
+        )
+      }
+
+      if (name === 'figure') {
+        return (
+          <figure className={styles.figure}>
+            {domToReact(children, options)}
+          </figure>
+        )
+      }
+
+      if (name === 'img') {
+        const img = <img src={`http://www.lunn.ru${attribs.src}`} className={styles.img} />
+
+        // if (!parent || (parent.name !== 'figure' && parent.name !== 'p')) {
+        //   img = <figure>{img}</figure>
+        // }
+
+        return img
+      }
+
+      if (name === 'h1' || name === 'h2') {
+        return (
+          <h2 className={styles.h2}>
+            {domToReact(children, options)}
+          </h2>
+        )
+      }
+
+      if (name === 'strong') {
+        if (parent && parent.type === 'tag' && (parent.name === 'h2' || parent.name === 'strong')) {
+          return (
+            <>
+              {domToReact(children, options)}
+            </>
+          )
+        }
+
+        return (
+          <strong>
+            {domToReact(children, options)}
+          </strong>
+        )
+      }
+
+      if (name === 'ul') {
+        return (
+          <ul className={styles.ul}>
+            {domToReact(children, options)}
+          </ul>
+        )
+      }
+
+      if (name === 'ol') {
+        return (
+          <ol className={styles.ol}>
+            {domToReact(children, options)}
+          </ol>
+        )
+      }
+
+      if (name === 'li') {
+        return (
+          <li>
+            {domToReact(children, options)}
+          </li>
+        )
+      }
+    }
+
+    //
+    // if (attribs.class === 'prettify') {
+    //   return (
+    //     <span style={{ color: 'hotpink' }}>
+    //       {domToReact(children, options)}
+    //     </span>
+    //   )
+    // }
   }
 }
+
+// export default function Renderer ({ source }) {
+//   const blocks = convertFromHTML(source)
+//   const state = ContentState.createFromBlockArray(
+//     blocks.contentBlocks,
+//     blocks.entityMap
+//   )
+//
+//   return redraft(convertToRaw(state), renderers)
+// }
+//
+// const getParagraphs = (children, { keys }) =>
+//   children.map((p, i) =>
+//     <p key={keys[i]} className={styles.p}>
+//       {p}
+//     </p>
+//   )
+//
+// const getH2 = (children, { keys }) =>
+//   children.map((p, i) =>
+//     <h2 key={keys[i]} className={styles.h2}>
+//       {p}
+//     </h2>
+//   )
+//
+// const getUl = (children, { depth, keys }) =>
+//   <ul key={keys.join('-')} className={styles.ul}>
+//     {children.map((li, i) =>
+//       <li key={keys[i]}>{li}</li>
+//     )}
+//   </ul>
+//
+// const getOl = (children, { depth, keys }) =>
+//   <ol key={keys.join('-')} className={styles.ol}>
+//     {children.map((li, i) =>
+//       <li key={keys[i]}>{li}</li>
+//     )}
+//   </ol>
+//
+// const getStyle = (children, { key }) =>
+//   <strong key={key}>
+//     {children}
+//   </strong>
+//
+// const renderers = {
+//   inline: {
+//     BOLD: getStyle
+//   },
+//   blocks: {
+//     unstyled: getParagraphs,
+//     'header-two': getH2,
+//     'unordered-list-item': getUl,
+//     'ordered-list-item': getOl
+//   }
+// }
