871823d0f1bd84909b978a526191d4051743aabf
Andy Braga <ab@luckypike.com>
2020-01-15 12:39:41 +0300

    Меньше релизов

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ append :linked_files, 'config/database.yml', 'config/master.key'
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
-set :keep_releases, 5
+set :keep_releases, 3
 
 set :ssh_options, forward_agent: true
 
