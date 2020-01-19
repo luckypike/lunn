9d8014a321218d35ffd3072643cab5f94a640c63
Andy Braga <bragovo@gmail.com>
2020-01-19 11:45:18 +0300

    Вывод текста о сотрудниках

--- app/javascript/components/Tutors/Show.js
+++ app/javascript/components/Tutors/Show.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
-import { Link } from '@reach/router'
+// import { Link } from '@reach/router'
 
 import { Title } from '../Pages'
+import Renderer from '../Draft'
 
-import styles from './Index.module.css'
+import styles from './Show.module.css'
 import pages from '../Pages.module.css'
 
 Show.propTypes = {
@@ -28,9 +29,33 @@ export default function Show ({ id }) {
   return (
     <div className={pages.container}>
       {node &&
-        <Title
-          h1={node.title}
-        />
+        <>
+          <Title
+            h1={node.title}
+          />
+
+          <div className={styles.root}>
+            <div className={styles.photo}>
+              <div className={styles.image}>
+                {node.image &&
+                  <img src={node.image.path} />
+                }
+              </div>
+            </div>
+
+            {node.desc &&
+              <div className={styles.desc}>
+                <Renderer source={node.desc} />
+              </div>
+            }
+
+            {node.text &&
+              <div className={styles.text}>
+                <Renderer source={node.text} />
+              </div>
+            }
+          </div>
+        </>
       }
     </div>
   )
