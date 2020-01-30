4d16a7d15e0c70989752057b773b736df681ccee
Maxim <max.spiridonov@gmail.com>
2020-01-30 17:53:51 +0300

    Добавлен ScrollToTop в компоненты Events/News

--- app/javascript/components/Events/Events.js
+++ app/javascript/components/Events/Events.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Router } from '@reach/router'
 
+import ScrollToTop from '../ScrollToTop'
+
 import Index from './Index'
 import Show from './Show'
 
 export default function Events () {
   return (
     <Router>
-      <Index path="events" />
-      <Show path="events/:slug" />
+      <ScrollToTop path="/">
+        <Index path="events" />
+        <Show path="events/:slug" />
+      </ScrollToTop>
     </Router>
   )
 }
--- app/javascript/components/News/News.js
+++ app/javascript/components/News/News.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Router } from '@reach/router'
 
+import ScrollToTop from '../ScrollToTop'
+
 import Index from './Index'
 import Show from './Show'
 
 export default function News () {
   return (
     <Router>
-      <Index path="news" />
-      <Show path="news/:slug" />
+      <ScrollToTop path="/">
+        <Index path="news" />
+        <Show path="news/:slug" />
+      </ScrollToTop>
     </Router>
   )
 }
