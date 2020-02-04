a6359b3cec1a7b0954bb71cea66f293db908fb73
Maxim <max.spiridonov@gmail.com>
2020-02-04 16:39:43 +0300

    Изменен вид на большой у некоторых пунктов меню

--- app/javascript/components/Pages/Show.js
+++ app/javascript/components/Pages/Show.js
@@ -22,7 +22,7 @@ Show.propTypes = {
 }
 
 export default function Show ({ node, navs, docs, loaf, tutors, locale }) {
-  const arr = [5260, 480, 475, 490, 493, 491, 492, 498, 494, 495, 476]
+  const arr = [5260, 480, 475, 490, 493, 491, 492, 498, 494, 495, 476, 3516, 323, 324, 325, 1384, 4512, 5224]
 
   return (
     <div className={classNames(styles.root, pages.container)}>
