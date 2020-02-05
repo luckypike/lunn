1051483a6db968d5adefa0678f69074b22ece703
Andy Braga <ab@luckypike.com>
2020-02-05 13:34:33 +0300

    Подправлен компонент скролла

--- app/javascript/components/ScrollToTop.js
+++ app/javascript/components/ScrollToTop.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useEffect } from 'react'
 
 export default function ScrollToTop ({ children, location }) {
-  React.useEffect(() => window.scrollTo({ top: 0, behavior: 'smooth' }), [location.pathname + location.search])
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [location.pathname, location.search])
+
   return children
 }
