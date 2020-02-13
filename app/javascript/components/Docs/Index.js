d1d0d454156f31a4011ba09720e979ac904ca6ae
Maxim <max.spiridonov@gmail.com>
2020-02-13 09:41:24 +0300

    Убран лишний стейт из поиска на странице документов

--- app/javascript/components/Docs/Index.js
+++ app/javascript/components/Docs/Index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import mime from 'mime-types'
@@ -20,19 +20,10 @@ Show.propTypes = {
 export default function Show ({ node, docs, loaf, locale }) {
   const I18n = useI18n(locale)
 
-  const [searchTerm, setSearchTerm] = useState('')
-  const [searchResults, setSearchResults] = useState([])
-
-  useEffect(() => {
-    const results = docs.filter(doc =>
-      doc.title.toString().toLowerCase().includes(searchTerm.toString().toLowerCase())
-    )
-
-    setSearchResults(results)
-  }, [searchTerm])
+  const [search, setSearch] = useState('')
 
   const handleChange = (e) => {
-    setSearchTerm(e.target.value)
+    setSearch(e.target.value)
   }
 
   return (
@@ -52,27 +43,27 @@ export default function Show ({ node, docs, loaf, locale }) {
         <input
           type="text"
           placeholder="Введите название документа для поиска..."
-          value={searchTerm}
+          value={search}
           onChange={handleChange}
         />
       </div>
 
-      {searchResults &&
+      {docs &&
         <ul className={styles.docs}>
-          {searchResults.map(searchResult =>
-            <li key={searchResult.fid}>
-              <a href={searchResult.path} target="_blank" rel="noopener noreferrer" className={styles.doc} itemProp={searchResult.wrapper}>
+          {docs.filter(d => d.title.toLowerCase().includes(search.toLowerCase())).map(doc =>
+            <li key={doc.fid}>
+              <a href={doc.path} target="_blank" rel="noopener noreferrer" className={styles.doc} itemProp={doc.wrapper}>
                 <svg viewBox="0 0 32 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M20 0H4A3.995 3.995 0 00.02 4L0 36a3.994 3.994 0 003.98 4H28a4.011 4.011 0 004-4V12L20 0zM4 36V4h14v10h10v22H4z" fill="#2F53EB"/>
                 </svg>
 
                 <div className={styles.data}>
                   <div className={styles.title}>
-                    {searchResult.title}
+                    {doc.title}
                   </div>
 
                   <div className={styles.meta}>
-                    ({mime.extension(searchResult.mime)}, {I18n.toHumanSize(searchResult.size, { precision: 1, format: '%n %u', strip_insignificant_zeros: true })})
+                    ({mime.extension(doc.mime)}, {I18n.toHumanSize(doc.size, { precision: 1, format: '%n %u', strip_insignificant_zeros: true })})
                   </div>
 
                 </div>
