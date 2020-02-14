3741c014c6737edb38f60c9b877ed3a36e7ef4a7
Andy Braga <ab@luckypike.com>
2020-02-14 14:05:17 +0300

    Попытка сделать defer лишнего

--- app/javascript/components/Pages/Index/Youtube.js
+++ app/javascript/components/Pages/Index/Youtube.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
 
 import styles from './Youtube.module.css'
+
+Frame.propTypes = {
+  src: PropTypes.string,
+  title: PropTypes.string,
+  desc: PropTypes.string
+}
 
 export default function Youtube () {
   return (
@@ -9,32 +16,40 @@ export default function Youtube () {
         Видеоновости
       </div>
 
-      <div className={styles.video}>
-        <div className={styles.youtube}>
-          <iframe src='https://www.youtube.com/embed/gr9xa9pdpDk' frameBorder="0" allowFullScreen={true} />
-        </div>
+      <Frame
+        src="https://www.youtube.com/embed/gr9xa9pdpDk"
+        title="Ректор НГЛУ Жанна Никонова поздравляет коллег с Днем российской науки"
+        desc="7 февраля 2020"
+      />
 
-        <div className={styles.title}>
-          Ректор НГЛУ Жанна Никонова поздравляет коллег с Днем российской науки
-        </div>
+      <Frame
+        src="https://www.youtube.com/embed/CDYozEbzF0s"
+        title="Ученые НГЛУ поздравляют коллег с Днем российской науки"
+        desc="7 февраля 2020"
+      />
+    </div>
+  )
+}
 
-        <div className={styles.date}>
-          7 февраля 2020
-        </div>
+function Frame ({ src, title, desc }) {
+  const frameRef = useRef()
+
+  useEffect(() => {
+    frameRef.current.setAttribute('src', src)
+  }, [])
+
+  return (
+    <div className={styles.video}>
+      <div className={styles.youtube}>
+        <iframe ref={frameRef} frameBorder="0" allowFullScreen={true} />
       </div>
 
-      <div className={styles.video}>
-        <div className={styles.youtube}>
-          <iframe src='https://www.youtube.com/embed/CDYozEbzF0s' frameBorder="0" allowFullScreen={true} />
-        </div>
+      <div className={styles.title}>
+        {title}
+      </div>
 
-        <div className={styles.title}>
-          Ученые НГЛУ поздравляют коллег с Днем российской науки
-        </div>
-
-        <div className={styles.date}>
-          7 февраля 2020
-        </div>
+      <div className={styles.date}>
+        {desc}
       </div>
     </div>
   )
