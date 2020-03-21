66f1f511c3900dd3553bdd2a43f8c69559a3c024
Andy Braga <bragovo@gmail.com>
2020-03-21 19:12:04 +0300

    Кеширование nav элементов на 30 минут

--- app/serializers/nav_serializer.rb
+++ app/serializers/nav_serializer.rb
@@ -1,5 +1,7 @@
 class NavSerializer
   include FastJsonapi::ObjectSerializer
 
+  cache_options enabled: true, cache_length: 30.minutes
+
   attributes :path, :title, :url, :depth, :mlid, :plid
 end
