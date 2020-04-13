2e43a5a63896ef3f01b8b130462ded0a6d406f1d
Andy Braga <ab@luckypike.com>
2020-04-13 14:39:10 +0300

    Видео на главной инициализируются после клика

--- app/javascript/components/Pages/Index/Youtube.js
+++ app/javascript/components/Pages/Index/Youtube.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import styles from './Youtube.module.css'
 
 Frame.propTypes = {
-  src: PropTypes.string,
+  id: PropTypes.string,
   title: PropTypes.string,
   desc: PropTypes.string
 }
@@ -17,13 +18,13 @@ export default function Youtube () {
       </div>
 
       <Frame
-        src="https://www.youtube.com/embed/lLZ6hGXlsEM"
+        id="lLZ6hGXlsEM"
         title="Видеообращение ректора НГЛУ им. Н.А.Добролюбова Жанны Никоновой"
         desc="6 апреля 2020"
       />
 
       <Frame
-        src="https://www.youtube.com/embed/XUkJUeL4wFQ"
+        id="XUkJUeL4wFQ"
         title="Отзыв об НГЛУ Константин Кириченко"
         desc="19 февраля 2020"
       />
@@ -31,17 +32,24 @@ export default function Youtube () {
   )
 }
 
-function Frame ({ src, title, desc }) {
-  const frameRef = useRef()
-
-  useEffect(() => {
-    frameRef.current.setAttribute('src', src)
-  }, [])
+function Frame ({ id, title, desc }) {
+  const [active, setActive] = useState(false)
 
   return (
     <div className={styles.video}>
-      <div className={styles.youtube}>
-        <iframe ref={frameRef} frameBorder="0" allowFullScreen={true} />
+      <div className={classNames(styles.youtube, { [styles.active]: active })} onClick={() => setActive(true)} >
+        {!active &&
+          <img src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`} />
+        }
+
+        {active &&
+          <iframe
+            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay"
+          />
+        }
       </div>
 
       <div className={styles.title}>
