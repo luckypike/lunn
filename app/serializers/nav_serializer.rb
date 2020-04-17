41939a80cc53ff1de191b08a701fd4f962f1dc30
Andy Braga <ab@luckypike.com>
2020-04-17 10:02:25 +0300

    Настройки кеша для 1.6 версии

--- app/serializers/nav_serializer.rb
+++ app/serializers/nav_serializer.rb
@@ -1,7 +1,7 @@
 class NavSerializer
   include FastJsonapi::ObjectSerializer
 
-  cache_options enabled: true, cache_length: 30.minutes
+  cache_options store: Rails.cache, namespace: 'fast-jsonapi', expires_in: 30.minutes
 
   attributes :path, :title, :url, :depth, :mlid, :plid
 end
