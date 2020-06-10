ed4a13313b3193e9db075131c59b756a0daa8a80
Andy Braga <ab@luckypike.com>
2020-06-10 22:18:00 +0300

    Поправлен блок поступления

--- app/javascript/components/Pages/Abitur/Admission.js
+++ app/javascript/components/Pages/Abitur/Admission.js
@@ -5,20 +5,32 @@ import styles from './Admission.module.css'
 export default function Admission () {
   return (
     <div className={styles.root}>
-      <h2>
-        Поступай легко — поступай онлайн
-      </h2>
+      <div className={styles.online}>
+        <h2>
+          Поступай легко — поступай онлайн
+        </h2>
+      </div>
 
-      <div>
+      <div className={styles.account}>
         <p className={styles.desc}>
           В 2020 году прием документов мы будет осуществлять только в дистанционной форме, через личный кабинет абитуриента на нашем сайте, который откроется для вас 19 июня 2020 года.
         </p>
 
-        <p className={styles.action}>
+        <p className={styles.how}>
           <a className={styles.button} href="/abitur/online">
             Как поступить в НГЛУ?
           </a>
+        </p>
 
+        <h3>
+          Личный кабинет абитуриента
+        </h3>
+
+        <p>
+          Здесь вы можете подать заявление о приёме и документы
+        </p>
+
+        <p>
           <a className={styles.button} href="/admissions/new">
             Подать документы
           </a>
