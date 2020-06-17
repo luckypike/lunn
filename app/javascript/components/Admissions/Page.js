0ff2e1fa799b446c7eee0bb1d98837d276a0ff7f
Andy Braga <ab@luckypike.com>
2020-06-17 17:18:10 +0300

    Вывод документов

--- app/javascript/components/Admissions/Page.js
+++ app/javascript/components/Admissions/Page.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { deserialize } from 'jsonapi-deserializer'
 // import classNames from 'classnames'
 
 // import { Title } from '../Pages'
 import Title from '../Title'
+import Docs from '../Docs/Docs'
 import Renderer from '../Renderer'
 
 import styles from './Page.module.css'
@@ -11,12 +13,15 @@ import pages from '../Pages.module.css'
 
 Page.propTypes = {
   node: PropTypes.object,
+  docs: PropTypes.object,
   user: PropTypes.object,
   loaf: PropTypes.array,
   locale: PropTypes.string
 }
 
-export default function Page ({ node, user, loaf, locale }) {
+export default function Page ({ node, docs: docsJSON, user, loaf, locale }) {
+  const docs = deserialize(docsJSON)
+
   return (
     <div className={pages.beta}>
       <Title
@@ -29,6 +34,12 @@ export default function Page ({ node, user, loaf, locale }) {
         {node.text &&
           <div className={styles.text}>
             <Renderer source={node.text} />
+          </div>
+        }
+
+        {docs &&
+          <div className={styles.docs}>
+            <Docs docs={docs} locale={locale} />
           </div>
         }
       </div>
