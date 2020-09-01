4d410f5df6110c5d69be06680826c18851d754b0
Andy Braga <bragovo@gmail.com>
2020-09-01 12:39:06 +0300

    Скрывать неопубликованные файлы

--- app/models/node/doc.rb
+++ app/models/node/doc.rb
@@ -2,7 +2,7 @@ class Node::Doc < ApplicationRecord
   self.table_name = 'field_data_field_attachment'
   self.primary_key = 'field_attachment_fid'
 
-  default_scope { order(delta: :asc) }
+  default_scope { order(delta: :asc).where(field_attachment_display: 1) }
 
   belongs_to :attachment, -> { includes(:attachment_wrapper) }, foreign_key: :field_attachment_fid
   belongs_to :node, -> { where(entity_type: :node) }, foreign_key: :entity_id, inverse_of: :docs
@@ -22,5 +22,9 @@ class Node::Doc < ApplicationRecord
 
   def mime
     attachment.filemime
+  end
+
+  def display
+    field_attachment_display
   end
 end
