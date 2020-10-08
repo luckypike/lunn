c741e4aa033d4332c21cfaa63c6557d7b57c54c4
Andy Braga <bragovo@gmail.com>
2020-10-09 01:07:20 +0300

    Перевод контактов

--- app/javascript/components/Pages/Contacts.js
+++ app/javascript/components/Pages/Contacts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { YMaps, Map, Placemark } from 'react-yandex-maps'
+import { useI18n, I18nContext } from '../I18n'
 
 import Ballon from '../Pages/Index/Images/ballon.png'
 
@@ -10,101 +11,107 @@ import styles from './Contacts.module.css'
 import pages from '../Pages.module.css'
 
 Contacts.propTypes = {
-  contacts: PropTypes.array
+  address: PropTypes.string,
+  contacts: PropTypes.array,
+  locale: PropTypes.string
 }
 
-export default function Contacts ({ contacts }) {
+export default function Contacts ({ address, contacts, locale }) {
+  const I18n = useI18n(locale)
+
   return (
-    <div className={pages.container}>
-      <Title
-        h1="Контакты"
-        loaf={[
-          {
-            mlid: 99999,
-            path: 'about',
-            title: 'Об Университете'
-          },
-          {
-            mlid: 99998,
-            path: 'contacts',
-            title: 'Контакты'
-          }
-        ]}
-      />
+    <I18nContext.Provider value={I18n}>
+      <div className={pages.container}>
+        <Title
+          h1={I18n.t('pages.contacts.title')}
+          loaf={[
+            {
+              mlid: 99999,
+              path: 'about',
+              title: I18n.t('pages.contacts.about')
+            },
+            {
+              mlid: 99998,
+              path: 'contacts',
+              title: I18n.t('pages.contacts.title')
+            }
+          ]}
+        />
 
-      <div className={styles.root}>
-        <div className={styles.map}>
-          <div className={styles.placeholder} />
+        <div className={styles.root}>
+          <div className={styles.map}>
+            <div className={styles.placeholder} />
 
-          <YMaps>
-            <Map className={styles.ymap} defaultState={{ center: [56.325030, 44.031256], zoom: 16 }}>
-              <Placemark
-                defaultGeometry={[56.32462542625421, 44.029508920558776]}
-                options = {{
-                  iconLayout: 'default#image',
-                  iconImageHref: Ballon,
-                  iconImageSize: [45, 50],
-                  iconImageOffset: [-20, -50]
-                }}
-              />
-            </Map>
-          </YMaps>
-        </div>
+            <YMaps>
+              <Map className={styles.ymap} defaultState={{ center: [56.325030, 44.031256], zoom: 16 }}>
+                <Placemark
+                  defaultGeometry={[56.32462542625421, 44.029508920558776]}
+                  options = {{
+                    iconLayout: 'default#image',
+                    iconImageHref: Ballon,
+                    iconImageSize: [45, 50],
+                    iconImageOffset: [-20, -50]
+                  }}
+                />
+              </Map>
+            </YMaps>
+          </div>
 
-        <div className={styles.items}>
-          <div className={styles.item}>
-            <span className={styles.text}>Адрес:</span>
-            <div className={styles.address}>
-              603155, Россия, Нижний Новгород, ул. Минина 31а
+          <div className={styles.items}>
+            <div className={styles.item}>
+              <span className={styles.text}>Адрес:</span>
+              <div className={styles.address}>
+                {address}
+              </div>
+            </div>
+
+            <div className={styles.item}>
+              <span className={styles.text}>Телефон:</span>
+              <a href="tel:+78314361575" className={styles.tel}>
+                +7 (831) 436-15-75
+              </a>
+            </div>
+
+            <div className={styles.item}>
+              <span className={styles.text}>Факс:</span>
+              <a href="tel:+78314166131" className={styles.tel}>
+                +7 (831) 416-61-31
+              </a>
+            </div>
+
+            <div className={styles.item}>
+              <span className={styles.text}>Эл. почта:</span>
+              <a href="mailto:admdep@lunn.ru" className={styles.button}>
+                admdep@lunn.ru
+              </a>
             </div>
           </div>
 
-          <div className={styles.item}>
-            <span className={styles.text}>Телефон:</span>
-            <a href="tel:+78314361575" className={styles.tel}>
-              +7 (831) 436-15-75
-            </a>
-          </div>
+          <div className={styles.phones}>
+            <h2>
+              {I18n.t('pages.contacts.phones')}
+            </h2>
 
-          <div className={styles.item}>
-            <span className={styles.text}>Факс:</span>
-            <a href="tel:+78314166131" className={styles.tel}>
-              +7 (831) 416-61-31
-            </a>
-          </div>
+            <div className={styles.contacts}>
+              {contacts.map((contact, i) =>
+                <div key={i} className={styles.contact}>
+                  <div className={styles.position}>
+                    {contact[0]}
+                  </div>
 
-          <div className={styles.item}>
-            <span className={styles.text}>Эл. почта:</span>
-            <a href="mailto:admdep@lunn.ru" className={styles.button}>
-              admdep@lunn.ru
-            </a>
-          </div>
-        </div>
+                  <div className={styles.title}>
+                    {contact[1]}
+                  </div>
 
-        <div className={styles.phones}>
-          <h2>
-            Краткий телефонный справочник
-          </h2>
-
-          <div className={styles.contacts}>
-            {contacts.map((contact, i) =>
-              <div key={i} className={styles.contact}>
-                <div className={styles.position}>
-                  {contact[0]}
+                  <div className={styles.phone}>
+                    {contact[2]}
+                  </div>
                 </div>
-
-                <div className={styles.title}>
-                  {contact[1]}
-                </div>
-
-                <div className={styles.phone}>
-                  {contact[2]}
-                </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </I18nContext.Provider>
   )
 }
--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,7 +1,7 @@
 class Variable < ApplicationRecord
-  self.table_name = 'variable'
+  self.table_name = 'variable_store'
 
-  after_find do
-    self.value = PHP.unserialize(value).force_encoding('utf-8')
-  end
+  # after_find do
+  #   self.value = PHP.unserialize(value).force_encoding('utf-8')
+  # end
 end
